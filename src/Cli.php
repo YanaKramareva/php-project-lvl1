8b0494a50f6e8a83b399064bfb689f782afae842
YanaKramareva <iana2000@mail.ru>
2021-10-31 17:31:47 +0300

    Update 4 files

--- src/Cli.php
+++ src/Cli.php
@@ -6,7 +6,8 @@ use function cli\line;
 use function cli\prompt;
 
 function getName()
-{line('Welcome to the Brain Game!');
+{
+    line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line('Hello, %s!', $name);
 }
