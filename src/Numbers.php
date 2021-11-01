2eb2e16d8011fa16aea29856f844053e042db44b
YanaKramareva <iana2000@mail.ru>
2021-11-01 22:08:16 +0300

    Change Numbers.php

--- src/Numbers.php
+++ src/Numbers.php
@@ -15,19 +15,24 @@ function chooseNumber()
     $name = prompt('May I have your name?');
     line('Hello, %s!', $name);
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    $arrayNumbers = [date('m'), date('d'), date('y')];
+    $arrayNumbers = [date('H'), date('i'), date('s')];
+    $countCorrectAnswers = 0;
     foreach ($arrayNumbers as $Numbers) {
            $answer =  prompt('Question', $Numbers);
         if (($Numbers % 2 == 0 && $answer == 'yes') | ($Numbers % 2 != 0 && $answer == 'no')) {
             line('Correct!');
+            $countCorrectAnswers = $countCorrectAnswers + 1;
         } elseif ($Numbers % 2 == 0 && $answer != 'yes') {
-            line('"No" is wrong answer ;(. Correct answer was "yes".');
+            line('%s is wrong answer ;(. Correct answer was "yes".', $answer);
             line("Let's try again, %s!", $name);
             break;
-        } elseif ($Numbers % 2 != 0 && $answer == 'yes') {
-            line('"Yes" is wrong answer ;(. Correct answer was "no".');
+        } elseif ($Numbers % 2 != 0 && $answer != 'no') {
+            line('%s is wrong answer ;(. Correct answer was "no".', $answer);
             line("Let's try again, %s!", $name);
             break;
+        }
+        if ($countCorrectAnswers == 3) {
+            line('Congratulations, %s!', $name);
         }
     }
 }
