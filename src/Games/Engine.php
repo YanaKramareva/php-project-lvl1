ae453cecb66a5cc6a7126696051d0141dd28a82d
YanaKramareva <iana2000@mail.ru>
2021-11-03 12:49:50 +0300

    Change Engine.php to make without linter problems

--- src/Games/Engine.php
+++ src/Games/Engine.php
@@ -17,18 +17,35 @@ namespace Brain\Games\List_of_functions {
         return $user_name;
     }
 
+    /**
+     * Выбирает произвольные числа от 0 до 10
+     * @return array
+     */
     function choose_numbers(): array
     {
-        return $randomNumbers = [rand(0, 10), rand(0, 10)];
-    }
-    function ask_user_to_calculate($randomNumbers, $operation): string
-    {
-        $request = $randomNumbers[0] . $operation . $randomNumbers[1];
-        $userAnswer = prompt('Question', $request);
-        return $userAnswer;
+        return [rand(0, 10), rand(0, 10)];
     }
 
-    function compare_is_correct_answer($userAnswer, $correctAnswer)
+    /**
+     * Принимает на вход произвольные числа и операнд, формирует выражение, принимает ответ пользователя
+     * @param $randomNumbers
+     * @param $operand
+     * @return string
+     */
+    function ask_to_calculate($randomNumbers, $operand): string
+    {
+        $request = $randomNumbers[0] . $operand . $randomNumbers[1];
+        return prompt('Question', $request);
+    }
+
+    /**
+     * Сравнивает ответ пользователя с правильным ответом.
+     * При правильном ответе возвращает 1, иначе ноль
+     * @param $userAnswer
+     * @param $correctAnswer
+     * @return int
+     */
+    function is_correct_answer($userAnswer, $correctAnswer): int
     {
         $result = 0;
         if (strcasecmp($userAnswer, $correctAnswer) == 0) {
@@ -41,25 +58,41 @@ namespace Brain\Games\List_of_functions {
         return $result;
     }
 
+    /**
+     * Принимает на вход числа и операнд, возвращает результат.
+     * @param $randomNumbers
+     * @param $operand
+     * @return string
+     */
     function calculate_correct_answer($randomNumbers, $operand): string
     {
+        $correct_answer = '';
         if ($operand == '+') {
-            $correctAnswer = $randomNumbers[0] + $randomNumbers[1];
+            $correct_answer = $randomNumbers[0] + $randomNumbers[1];
         } elseif ($operand == '-') {
-            $correctAnswer = $randomNumbers[0] - $randomNumbers[1];
+            $correct_answer = $randomNumbers[0] - $randomNumbers[1];
         } elseif ($operand == '*') {
-            $correctAnswer = $randomNumbers[0] * $randomNumbers[1];
+            $correct_answer = $randomNumbers[0] * $randomNumbers[1];
         }
-        return $correctAnswer;
+        return $correct_answer;
     }
 
-    function choose_operation()
+    /**
+     * Выбирает произвольно математическую операцию.
+     * @return string
+     */
+    function choose_operation(): string
     {
-        $array_of_operands = ['+', '-', '*'];
-        $rand_key = array_rand($array_of_operands, 1);
-        return $operand = $array_of_operands[$rand_key];
+        $operands_array = ['+', '-', '*'];
+        $rand_key = array_rand($operands_array);
+        return $operands_array[$rand_key];
     }
 
+    /**
+     * @param $countCorrectAnswers
+     * @param $user_name
+     * Печатает результат работы
+     */
     function show_user_result($countCorrectAnswers, $user_name)
     {
         if ($countCorrectAnswers == true) {
