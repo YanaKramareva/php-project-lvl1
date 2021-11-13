831f83a3b5c58b98e7c75e56ec9e69c79a551db7
YanaKramareva <iana2000@mail.ru>
2021-11-13 09:14:57 +0300

    Changed brainPrime.php

--- src/Games/brainPrime.php
+++ src/Games/brainPrime.php
@@ -7,6 +7,9 @@ use Brain\Games\Engine;
 function brainPrime(int $is_simple_number): string
 {
     $correct_answer = 'yes';
+    if ($is_simple_number == 1) {
+        $correct_answer = 'no';
+    }
     for ($i = 2; $i < $is_simple_number; $i++) {
         if ($is_simple_number % $i == 0) {
             $correct_answer = 'no';
